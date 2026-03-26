bce80dd8102314fab56b07f0b8cc7628d172894f
MDingniD <s2578353@ed.ac.uk>
2026-03-25 23:42:11 -0700

    set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: 'https://MDinginD.github.io',
+  base: '/personal-math-website',
+});
